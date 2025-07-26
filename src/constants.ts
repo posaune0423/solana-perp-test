ab78b7d8cdf04b20b3a41b4795f9804da01165cb
Asuma Yamada <yamadaasuma@gmail.com>
2025-07-26 20:50:21 +0900

    fix

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const RPC_URL = "https://mainnet.helius-rpc.com/?api-key=fc611f77-f1f6-4594-8c71-ecf010ba1fab";
+export const RPC_URL = `https://mainnet.helius-rpc.com/?api-key=${process.env.HELIUS_API_KEY}`;
 export const USER_ADDRESS = "86oHY5tUBn4gqHSQAZJSUX6MU7WJVpg8bn8MZ4jKEb63";
